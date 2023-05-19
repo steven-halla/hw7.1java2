7ae75559f20278acfc4ba8b08439174dff287b39
steven-halla <steven.r.halla@gmail.com>
2023-05-19 13:57:01 -0700

    finished

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,33 +1,59 @@
+// Steven Halla
+// HW 7.1
+// CS 142
+
 import java.util.Scanner;
 
+/*******************************************************************
+ * Fibonacci: calculates the fib number at a specific index. This class also
+ *              tracks how many times we loop.
+ *
+ * @author Steven Halla
+ * @version 1.0
+ * @since 19 may 2023
+ **/
 public class Fibonacci {
     // Add a static variable to keep track of the number of calls
-    public static long numCalls = 0;
+    public static long loopNumber = 0;
 
-    /** Main method */
+    /*******************************************************************
+     * main: ask for user input for fib number.
+     *
+     * @param args String[]
+     * @author Steven Halla
+     * @version 1.0
+     * @since 19 may 2023
+     **/
     public static void main(String[] args) {
         // Create a Scanner
         Scanner input = new Scanner(System.in);
         System.out.print("Enter an index for a Fibonacci number: ");
         int index = input.nextInt();
 
-        // Find and display the Fibonacci number
         System.out.println("The Fibonacci number at index " + index + " is " + fib(index));
 
-        // Display the number of times fib was called
-        System.out.println("fib method was called " + numCalls + " times");
+        System.out.println("fib method was called " + loopNumber + " times");
     }
 
-    /** The method for finding the Fibonacci number */
+    /*******************************************************************
+     * fib: handles how many times we loop
+     *
+     * @param   index
+     * @return  fibonnci number at its index
+     * @author Steven Halla
+     * @version 1.0
+     * @since 19 may 2023
+     **/
+
     public static long fib(long index) {
         // Increment the count each time fib is called
-        numCalls++;
+        loopNumber++;
 
-        if (index == 0) // Base case
+        if (index == 0)
             return 0;
-        else if (index == 1) // Base case
+        else if (index == 1)
             return 1;
-        else // Reduction and recursive calls
+        else
             return fib(index - 1) + fib(index - 2);
     }
 }
--- src/SumSeries.java
+++ src/SumSeries.java
@@ -1,15 +1,43 @@
+// Steven Halla
+// HW 7.1
+// CS 142
+
+/*******************************************************************
+ * Fibonacci: calculates sum of a series for m(x) double uses recursion
+
+ * @author Steven Halla
+ * @version 1.0
+ * @since 19 may 2023
+ **/
 public class SumSeries {
+
+    /*******************************************************************
+     * main: for loop for our iterations, in this case 10
+
+     * @param args String[]
+     * @author Steven Halla
+     * @version 1.0
+     * @since 19 may 2023
+     **/
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.printf("m(%d) = %.2f\n", i, computeSeries(i));
         }
     }
 
+    /*******************************************************************
+     * computeSeries: calcualtes and calls computeSeries
+     * @param i int
+     * @return sum of our computeSeries
+     * @author Steven Halla
+     * @version 1.0
+     * @since 19 may 2023
+     **/
     public static double computeSeries(int i) {
         if (i == 1) {
-            return 1; // base case
+            return 1;
         } else {
-            return 1.0 / i + computeSeries(i - 1); // recursive call
+            return 1.0 / i + computeSeries(i - 1);
         }
     }
 }
